1208b96817c8814eb399e71792662759c9705665
vlevin <vlevin@azbukavkusa.ru>
2023-01-06 20:29:25 +0300

    Initial commit

---
+++ src/main/java/com/victorlevin/generatingfeignbyswaggertest/GeneratingFeignBySwaggerTestApplication.java
@@ -0,0 +1,13 @@
+package com.victorlevin.generatingfeignbyswaggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratingFeignBySwaggerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratingFeignBySwaggerTestApplication.class, args);
+    }
+
+}
